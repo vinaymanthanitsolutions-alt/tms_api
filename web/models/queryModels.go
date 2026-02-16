cd34bc544c993cac38baab57e934e3cbad713c56
Ayush Chauhan <chauhanayush0270@gmail.com>
2026-02-16 16:39:28 +0530

    query

---
+++ web/models/queryModels.go
@@ -0,0 +1,14 @@
+package models
+
+
+type Query struct {
+	ID          int     `json:"id"`
+	ProjectID   *string `json:"project_id"`
+	TaskID      *int    `json:"task_id"`
+	RaisedBy    string  `json:"raised_by"`
+	AssignedTo  *string `json:"assigned_to"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Priority    string  `json:"priority"`
+	Status      string  `json:"status"`
+}
